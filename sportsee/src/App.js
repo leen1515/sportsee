9225e64a39b5ddace5f9c08219bb921c244b50de
leen1515 <leventredeparis@gmail.com>
2023-08-30 19:24:43 +0200

    feat(): mock json + installation d3

--- sportsee/src/App.js
+++ sportsee/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userDatas, setUserDatas] = useState([]);
+  const [isDataLoading, setDataLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchDatas () {
+      try {
+        const response = await fetch('http://localhost:3000/datas/datasMocked.json');
+        const datasJson = await response.json();
+        setUserDatas(datasJson);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+      } finally {
+        setDataLoading(false);
+      }
+    }
+    fetchDatas();
+  }, []);
+  console.log(isDataLoading, error);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">{JSON.stringify(userDatas)}
     </div>
   );
 }
