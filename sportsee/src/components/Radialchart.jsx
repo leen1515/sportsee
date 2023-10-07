c27baa955d1e315c938f8e8b7dedd8ef90e4c531
leen1515 <leventredeparis@gmail.com>
2023-10-08 00:29:07 +0200

    feat(style): radial chart

--- sportsee/src/components/Radialchart.jsx
+++ sportsee/src/components/Radialchart.jsx
@@ -1,21 +1,33 @@
 import RadialchartD3 from './d3/RadialchartD3';
 import { useContext } from 'react';
 import { datasContext } from '../routes/GetRoutes';
+import styled from 'styled-components';
+
+const RadialchartContainer = styled.div`
+    width: 20%;
+    height:100%;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+`;
 
 function Radialchart() {
         const { datas } = useContext(datasContext);
         console.log("7 radialchart", datas?.userDatas?.todayScore)
-    return (
+    return (<RadialchartContainer>
         <svg id = "radialchartSvg" 
-        width="500px"
-        height="500px"
+        width="100%"
+        height="263px"
         viewBox="0 0 500 500"
-        style={{ backgroundColor: "#b6b6b6"}}
-    >
+        style = {{backgroundColor: '#FBFBFB', borderRadius: "8px"}}
+    >  <text width= "100px" x="32" y="100" fill="#ffffff87" fontSize="30px">
+    Score
+</text>
         {datas && (
             <RadialchartD3 data={ datas?.userDatas} />
         )}
-    </svg>)
+       
+    </svg></RadialchartContainer>)
 }
 
 export default Radialchart;
