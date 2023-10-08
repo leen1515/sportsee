08a37bbe048f06c4a4f0196295d7c4bf7f88ea47
leen1515 <leventredeparis@gmail.com>
2023-10-08 15:01:19 +0200

    fix(): tooltip red barchard add clean

--- sportsee/src/components/d3/BarchartD3.jsx
+++ sportsee/src/components/d3/BarchartD3.jsx
@@ -230,9 +230,10 @@ function BarChartD3({ data }) {
             .on('mouseover', mouseover)
             .on('mousemove', mousemove)
             .on('mouseleave', mouseleave);
-            
+
             return () => {
-                d3.select(svgRef.current).selectAll("*").remove();}
+                d3.select(svgRef.current).selectAll("*").remove();
+                d3.select('.infosBulle').remove();}
     }, [data]);
     return <svg ref={svgRef}></svg>;
 }
