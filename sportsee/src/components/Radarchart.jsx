8c49a5cd2330a2765d6638289ad1648270ac417f
leen1515 <leventredeparis@gmail.com>
2023-10-10 13:48:44 +0200

    fix(): position radarchart

--- sportsee/src/components/Radarchart.jsx
+++ sportsee/src/components/Radarchart.jsx
@@ -5,7 +5,8 @@ import styled from 'styled-components';
 
 const RadarchartContainer = styled.div`
     width: 28%;
-    height:100%;
+    height:auto;
+    aspect-ratio: 1/1;
     display:flex;
     align-items: center;
     justify-content: center;
@@ -21,7 +22,7 @@ function Radarchart() {
     return (
         <RadarchartContainer><svg id = "radarchartSvg" 
         width="100%"
-        height="263px"
+        height="100%"
         viewBox="0 0 500 500"
         style = {{backgroundColor: '#282D30', borderRadius: "5px"}}
     >
